ebdd8dd157469020f072a06f873f5e7420e84f60
Pageofme <150520121+Pageofme@users.noreply.github.com>
2024-02-28 15:50:41 +0700

    Add files via upload

--- js/design-your-web.js
+++ js/design-your-web.js
@@ -1,5 +1,6 @@
 function pickImage(){
-    options = ["fr1.jpg","fr3.jpg","me10.jpg","me12.jpg","fr5.jpg","me2.jpgs"];
+    options=["hoangdz.jpg"];
+    // options = ["me3.jpg","me11.jpg","me10.jpg","me12.jpg","me4.jpg","me2.jpgs"];
 randomImg = "images1/" + options[Math.floor(Math.random() * options.length)];
 img = document.querySelector("#header_img");
 img.setAttribute("src",randomImg);
